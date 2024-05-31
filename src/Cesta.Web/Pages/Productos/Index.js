7a9594cf88da786b81d149e78682da1bc835ba4a
Manuel Gonzalez <manuyako45@gmail.com>
2024-05-31 19:57:41 +0200

    Cesta pendiente

--- src/Cesta.Web/Pages/Productos/Index.js
+++ src/Cesta.Web/Pages/Productos/Index.js
@@ -12,10 +12,11 @@ $(async function () {
                         <div class="card-body cardProducto">
                             <img class="imagenProducto" src="data:image/png;base64,${record.imageBase64}" class="card-img-top" alt="${record.name}">
                             <h5 class="card-title">${record.name}</h5>
+                            <p class="card-text">Descripcion: ${record.description}</p>
                             <p class="card-text">Para: ${l('Enum:MascotaType.' + record.mascotaType)}</p>
                             <p class="card-text">Tipo: ${l('Enum:ProductoType.' + record.productoType)}</p>
-                            <p class="card-text">Precio: ${record.price}€</p>
-                            <button class="btnAgregarCesta" onclick="sumarNotificationCount(this)"> <i class="fas fa-shopping-basket"></i> Agregar a la cesta</button>
+                            <p class="card-text">Precio: ${record.price}&euro;</p>
+                            <button class="btnAgregarCesta"> <i class="fas fa-shopping-basket"></i> Agregar a la cesta</button>
                         </div>
                     </div>
                 </div>
@@ -33,83 +34,62 @@ $(async function () {
 
         // Muestra el GIF de carga
         $('#loadingGif').show();
+        $('#btn-notification').hide();
 
         // Espera de 2 segundos (2000 ms)
-        await new Promise(resolve => setTimeout(resolve, 10000));
+        await new Promise(resolve => setTimeout(resolve, 2500));
 
         cesta.productos.producto.getList(params).then(function (result) {
             renderCards(result);
             // Oculta el GIF de carga
             $('#loadingGif').hide();
+            $('#btn-notification').show();
         });
     }
 
-    await loadCards(); // Usa await para esperar a que loadCards se complete
+    await loadCards();
 
-    // Simula la actualización del número en el botón
-    function updateNotificationCount(count) {
-        const notificationCount = document.getElementById('notification-count');
-        notificationCount.textContent = count;
-    }
 
-    // Llama a la función para actualizar el número (ejemplo: 5)
-    updateNotificationCount(5);
-
-    sumarNotificationCount();
-
-    function sumarNotificationCount(boton) {
-        var cantidadElement = document.getElementById('notification-count');
-
-        console.log(cantidadElement);
-
-        // Obtener el valor actual de la cantidad
-        var cantidad = parseInt(cantidadElement.textContent);
-        // Sumar uno a la cantidad
-        cantidad++;
-        // Actualizar el texto mostrando la nueva cantidad
-        cantidadElement.textContent = cantidad;
-    }
-
-    function restarNotificationCount(boton) {
-        var cantidadElement = document.getElementById('notification-count');
-        // Obtener el valor actual de la cantidad
-        var cantidad = parseInt(cantidadElement.textContent);
-        // Verificar que la cantidad sea mayor que cero antes de restar
-        if (cantidad > 0) {
-            // Restar uno a la cantidad
-            cantidad--;
-            // Actualizar el texto mostrando la nueva cantidad
-            cantidadElement.textContent = cantidad;
-        }
-    }
-
-    const myButton = document.getElementById('btn-notification');
-    myButton.addEventListener('click', () => {
-        alert('Button clicked!');
-        // Aquí puedes añadir la función que deseas ejecutar
-    });
-
-    // Asegúrate de que los botones sean seleccionados después de que las tarjetas se hayan renderizado
     $(document).on('click', '.btnAgregarCesta', function () {
         var cantidadElement = document.getElementById('notification-count');
-
-        console.log(cantidadElement);
+        var cantidadPopUp = document.getElementById('notification-popup');
 
         // Obtener el valor actual de la cantidad
         var cantidad = parseInt(cantidadElement.textContent);
-        // Sumar uno a la cantidad
-        cantidad++;
-        // Actualizar el texto mostrando la nueva cantidad
-        cantidadElement.textContent = cantidad;
 
         // Alternar clase para cambiar estilo
         $(this).toggleClass('active');
 
-        // Cambiar el texto del botón (opcional)
+        // Cambiar el texto del botón y la cantidad de elementos según el estado del botón
         if ($(this).hasClass('active')) {
+            // Si el botón está activo, sumar uno a la cantidad
+            cantidad++;
             $(this).html('<i class="fas fa-check"></i> Agregado');
         } else {
+            // Si el botón no está activo, restar uno a la cantidad
+            cantidad--;
             $(this).html('<i class="fas fa-shopping-basket"></i> Agregar a la cesta');
         }
+
+        // Actualizar el texto mostrando la nueva cantidad
+        cantidadElement.textContent = cantidad;
+        cantidadPopUp.textContent = cantidad;
     });
+
+    
+    $(document).on('click', '#btn-notification', function () {
+        var popup = $('#notification-popup');
+
+        // Muestra la ventana emergente y anima su aparición
+        popup.stop().slideDown();
+
+        // Oculta la ventana emergente después de 2 segundos (2000 ms)
+        setTimeout(function () {
+            popup.stop().slideUp();
+        }, 2000);
+    });
+
+
+
+
 });
